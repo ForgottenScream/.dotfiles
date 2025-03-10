51010c28eca989b79bbf310fc6336e4271f874b2
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-10 01:18:49 +0000

    updated to include zsh and tmux

--- install.sh
+++ install.sh
@@ -5,3 +5,19 @@
 ######
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/.dotfiles/i3" "$HOME/.config"
+
+#######
+# ZSH #
+#######
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/.dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/.dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+rm -rf "$HOME/.config/zsh/external"
+ln -sf "$HOME/.dotfiles/zsh/external" "$HOME/.config/zsh"
+
+########
+# TMUX #
+########
+mkdir -p "$HOME/.config/tmux"
+ln -sf "$HOME/.dotfiles/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
