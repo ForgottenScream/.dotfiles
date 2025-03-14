50bdd5de1f4f4f05539bb22ea7338a701132feca
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-14 00:08:37 +0000

    testing

--- install.sh
+++ install.sh
@@ -3,40 +3,40 @@
 # X11 #
 #######
 rm -rf "$HOME/.config/X11"
-ln -s "$DOTFILES/X11" "$HOME/.config"
-#ln -s "$DOTFILES/X11/.xinitrc" "$HOME"
-#ln -s "$DOTFILES/X11/.Xresources" "$HOME"
+ln -s "$HOME/.dotfiles/X11" "$HOME/.config"
+#ln -s "$HOME/.dotfiles/X11/.xinitrc" "$HOME"
+#ln -s "$HOME/.dotfiles/X11/.Xresources" "$HOME"
 
 ######
 # i3 #
 ######
 rm -rf "$HOME/.config/i3"
-ln -s "$DOTFILES/i3" "$HOME/.config"
+ln -s "$HOME/.dotfiles/i3" "$HOME/.config"
 
 #######
 # ZSH #
 #######
 mkdir -p "$HOME/.config/zsh"
-ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.config/zsh"
-ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
-ln -sf "$DOTFILES/zsh/aliases" "$HOME/.config/zsh/aliases"
+ln -sf "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/.dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/.dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
 rm -rf "$HOME/.config/zsh/external"
-ln -sf "$DOTFILES/zsh/external" "$HOME/.config/zsh"
+ln -sf "$HOME/.dotfiles/zsh/external" "$HOME/.config/zsh"
 
 ########
 # TMUX #
 ########
 #mkdir -p "$HOME/.config/tmux"
-#ln -sf "$DOTFILES/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+#ln -sf "$HOME/.dotfiles/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
 
 #########
 # dunst #
 #########
 mkdir -p "$HOME/.config/dunst"
-ln -sf "$DOTFILES/dunst/dunstrc" "$HOME/.config/dunst/dunstrc"
+ln -sf "$HOME/.dotfiles/dunst/dunstrc" "$HOME/.config/dunst/dunstrc"
 
 #########
 # Fonts #
 #########
 # mkdir -p "$XDG_DATA_HOME"
-# cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+# cp -rf "$HOME/.dotfiles/fonts" "$XDG_DATA_HOME"
